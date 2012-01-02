b8170df4ffad32850abaa976ca956ae5eec5241d
Oka Motofumi <chikuzen.mo@gmail.com>
2012-01-03 07:59:57 +0900

    update copyright

--- en_resource.c
+++ en_resource.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1999-2011 Kenkun
+Copyright (c) 1999-2012 Kenkun
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
